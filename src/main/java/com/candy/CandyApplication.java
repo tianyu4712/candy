44545657f2e98c35db5315abeff5cd6a4e3831d3
tianyu4712@163.com <tianyu4712@163.com>
2019-04-13 22:23:41 +0800

    初始化项目

---
+++ src/main/java/com/candy/CandyApplication.java
@@ -0,0 +1,13 @@
+package com.candy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/candy/CandyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.candy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CandyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
